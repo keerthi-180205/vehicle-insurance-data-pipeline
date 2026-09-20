0b1ee5c48dd1a6da0ff94f90dbf2157bbac4477a
keerthi-180205 <keerthin180205@gmail.com>
2026-09-20 21:17:27 +0530

    project statue - localhost

---
+++ demo.py
@@ -0,0 +1,12 @@
+from src.logger import logging
+
+logging.debug("This is a debug message.")
+logging.info("This is an info message.")
+logging.warning("This is a warning message.")
+logging.error("This is an error message.")
+logging.critical("This is a critical message.")
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
